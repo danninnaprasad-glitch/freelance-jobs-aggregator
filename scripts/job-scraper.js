fba80695dc4f7b4802b2dee39f49eeb06ffe3a6c
Siva <danninnaprasad@gmail.com>
2025-10-15 14:47:00 +0530

    Delete scripts directory

--- scripts/job-scraper.js
+++
@@ -1,8 +0,0 @@
-const Parser = require('rss-parser');
-const fs = require('fs');
-const path = require('path');
-
-const parser = new Parser();
-const jobsFile = path.join(__dirname, '../data/jobs.json'); // Changed to root data folder
-
-// Your existing scraper code continues...
